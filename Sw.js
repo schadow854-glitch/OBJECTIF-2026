6ffc5cd7b391d1a01ccf2dcb06d8fa0b41cc71ae
schadow854-glitch <schadow854@gmail.com>
2026-05-10 09:47:48 +0100

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'mon-plan-2026-v3';
+const FILES = ['/', '/index.html', '/manifest.json', '/icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
